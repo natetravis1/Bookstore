47d0e5dcfb8a7df5392e43d8f171553e5d047284
natetravis1 <90298840+natetravis1@users.noreply.github.com>
2022-02-16 23:24:00 -0700

    first

---
+++ Bookstore/Models/ViewModels/PageInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int TotalNumBooks { get; set; }
+        public int BooksPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        // calculates how many pages we'll need
+        public int TotalPages => (int) Math.Ceiling((double) TotalNumBooks / BooksPerPage);
+    }
+}
