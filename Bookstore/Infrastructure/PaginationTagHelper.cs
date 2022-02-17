ae8372c57ab1fd8039446ef322354e5ab6a739a7
natetravis1 <90298840+natetravis1@users.noreply.github.com>
2022-02-17 14:51:36 -0700

    styling

--- Bookstore/Infrastructure/PaginationTagHelper.cs
+++ Bookstore/Infrastructure/PaginationTagHelper.cs
@@ -31,6 +31,13 @@ namespace Bookstore.Infrastructure
         public PageInfo PageBlah { get; set; }
         public string PageAction { get; set; }
 
+        //for classes in this page:
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+
         public override void Process (TagHelperContext thc, TagHelperOutput tho)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
@@ -42,6 +49,12 @@ namespace Bookstore.Infrastructure
                 TagBuilder tb = new TagBuilder("a");
 
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageBlah.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
